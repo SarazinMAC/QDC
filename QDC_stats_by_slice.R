24f30fd9e61e30031784df63df0feae8cc740b24
sarazm <Marc.Sarazin@ons.gov.uk>
2024-06-15 11:56:45 +0100

    remove labels from + add calibri font to hist

--- QDC_stats_by_slice.R
+++ QDC_stats_by_slice.R
@@ -668,44 +668,67 @@ degree_data <- degree_data[nrow(degree_data),]
 
 degree_data <- as.data.frame(degree_data)
 
+# Note: no longer needed as node labels will be created elsewhere
 # create actor labels to display on the visualisation
-if (measure=="degree") {
-  actors_to_label <- c("Rousseau", "Mercure", "Année littéraire", "Rolland d'Erceville")
-  actor_labels <- c("Rousseau (16.7%)", " Mercure (8.6%)", " Année\nlittéraire (11.4%)", "Rolland\nd'Erceville (9.6%)")
-  # TODO: Change this so that label_y_position is used below as configurable value, also hjust, remember to change order of values
-#  label_y_position <- c(2, 5.5, 4, 2)
-} else if (measure=="outdegree") {
-  actors_to_label <- c("Rousseau", "Mercure", "Année littéraire", "Rolland d'Erceville", "Borrelly", "Rivard")
-  actor_labels <- c("Rousseau (0%)", "Mercure (8.4%)", "Année\nlittéraire (10.6%)", "Rolland\nd'Erceville (8.9%)", "Borrelly (5.3%)", "Rivard (7.6%)")
-#  label_y_position <- 1
-}
-
-degree_data$labels <- NA
-for (i in seq_along(actors_to_label)) {
-  degree_data[actors_to_label[i],"labels"] <- paste0(actor_labels[i])
-  degree_data[actors_to_label[i],"hjust"] <- paste0(hjust[i])
-}
+#if (text_or_pers=="pers") {
+#  if (measure=="degree") {
+#    actors_to_label <- c("Rousseau", "Mercure", "Année littéraire", "Rolland d'Erceville")
+#    actor_labels <- c("Rousseau (16.7%)", " Mercure (8.6%)", " Année\nlittéraire (11.4%)", "Rolland\nd'Erceville (9.6%)")
+#    label_y_position <- c(2, 2, 4, 5.5)
+#    label_hjust <- c(0.7, 0.5, 0.5, 0.5)
+#  } else if (measure=="outdegree") {
+#    actors_to_label <- c("Rousseau", "Mercure", "Année littéraire", "Rolland d'Erceville", "Borrelly", "Rivard")
+#    actor_labels <- c("Rousseau (0%)", "Mercure (8.4%)", "Année\nlittéraire (10.6%)", "Rolland\nd'Erceville (8.9%)", "Borrelly (5.3%)", "Rivard (7.6%)")
+#    label_y_position <- c(2.5, 4, 6, 7.5, 2, 2)
+#    label_hjust <- c(0.4, 0.5, 0.5, 0.5, 0.5, 0.5)
+#  }
+#} else if (text_or_pers=="text") {
+#  if (measure=="degree") {
+#    actors_to_label <- c("Rousseau (1762)", "D'Alembert (1753)", "Borrelly (1768)", "La Chalotais (1763)")
+#    actor_labels <- c("Rousseau\n(1762) - 16.6%", "D'Alembert\n(1753) - 6.2%", " Borrelly\n(1768) - 5.5%", "La Chalotais\n(1763) - 5.5%")
+#    label_y_position <- c(5, 5, 11, 11)
+#    label_hjust <- c(0.7, 0.1, -0.05, 1.05)
+#  } else if (measure=="outdegree") {
+#    actors_to_label <- c("Rousseau (1762)", "Borrelly (1768)", "Rolland d'Erceville (1783)", "Rolland d'Erceville (1769)")
+#    actor_labels <- c("Rousseau\n(1762) - 0%", "Borrelly\n(1768) - 5.2%", "Rolland d'Erceville\n(1783) - 4.9%", "Rolland d'Erceville\n(1769) - 3.6%")
+#    label_y_position <- c(5, 5, 12.5, 5)
+#    label_hjust <- c(0.4, 0.5, 0.5, 0.5)
+#  }
+#}
+#
+#degree_data$labels <- NA
+#degree_data$label_y_position <- NA
+#
+#for (i in seq_along(actors_to_label)) {
+#  degree_data[actors_to_label[i],"labels"] <- paste0(actor_labels[i])
+#  degree_data[actors_to_label[i],"label_y_position"] <- label_y_position[i]
+#  degree_data[actors_to_label[i],"label_hjust"] <- label_hjust[i]
+#}
+#
+#degree_data <- degree_data[order(degree_data$degree_data, decreasing = TRUE),]
 
 if (measure=="degree") {
   
 ggplot(data = degree_data,
        aes(x = degree_data)) +
   geom_histogram(binwidth = 1, fill = "#FF776C", col = "black") +
-  geom_label(data = degree_data[!is.na(degree_data$labels),], mapping = aes(x = degree_data, y =  c(2, 5.5, 4, 2), label = labels, hjust = c(0.7, 0.5, 0.5, 0.5)), size = 15) +
+#  geom_label(data = degree_data[!is.na(degree_data$labels),], mapping = aes(x = degree_data, y = label_y_position, label = labels, hjust = label_hjust), size = 15) +
   labs(x = "Degree centrality", y = "Number of nodes") +
   theme(axis.title = element_text(size = 50), axis.text = element_text(size = 50)) +
   scale_x_continuous(breaks = c(0, 10, 20, 30, 40, 50, 60)) +
-  theme(plot.margin = unit(c(15, 15, 15, 15), units = "pt"))
-} else if (measure=="outdegree") {
+  theme(plot.margin = unit(c(15, 15, 15, 15), units = "pt")) +
+  theme(text = element_text(family = "Calibri"))
   
+} else if (measure=="outdegree") {
   ggplot(data = degree_data,
          aes(x = degree_data)) +
     geom_histogram(binwidth = 1, fill = "#FF776C", col = "black") +
-    geom_label(data = degree_data[!is.na(degree_data$labels),], mapping = aes(x = degree_data, y =  c(7.5, 2, 6, 4, 2, 2.5), label = labels, hjust = c(0.5, 0.4, 0.5, 0.5, 0.5, 0.5)), size = 15) +
+#    geom_label(data = degree_data[!is.na(degree_data$labels),], mapping = aes(x = degree_data, y = label_y_position , label = labels, hjust = label_hjust), size = 15) +
     labs(x = "Degree centrality", y = "Number of nodes") +
     theme(axis.title = element_text(size = 50), axis.text = element_text(size = 50)) +
     scale_x_continuous(breaks = c(0, 10, 20, 30, 40, 50, 60)) +
-    theme(plot.margin = unit(c(15, 15, 15, 15), units = "pt"))
+    theme(plot.margin = unit(c(15, 15, 15, 15), units = "pt")) +
+    theme(text = element_text(family = "Calibri"))
 }
 
 # Export vis
