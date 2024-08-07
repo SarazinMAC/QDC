d4e2741ee91c2d84bcdcc4162df3f964afd98b09
SarazinMAC <sarazinmarc@gmail.com>
2024-08-07 15:22:38 +0100

    Finish production of table_1_results object

--- analysis_scripts/ad_hoc_analyses.R
+++ analysis_scripts/ad_hoc_analyses.R
@@ -15,6 +15,16 @@ if(text_or_pers=="pers") {
 # Produce results for Table 1
 
 table_1_results <- table(QDC_es$Quality)
+table_1_results <- as.numeric(table_1_results)
+names(table_1_results) <- c("An explicit negative reference (-2)",
+                                                          "An implicit negative reference (-1)",
+                                                          "A neutral reference (0)",
+                                                          "An implicit positive reference (1)",
+                                                          "An explicit positive reference (2)",
+                                                          "An ambivalent reference (3)",
+                                                          "A form of republication (4)",
+                                                          "A sequel or supplement (5)",
+                                                          "A response to a catalyst (6)")
 
 # Produce results about texts published in 1762 and 1763 and their references
 
