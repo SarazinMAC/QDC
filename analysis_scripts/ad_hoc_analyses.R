49a702873cf1f3729d774229475ae06ec9a6223e
SarazinMAC <sarazinmarc@gmail.com>
2024-08-10 11:51:19 +0100

    add ad-hoc analyses
    Add ad-hoc analyses for indegrees and outdegrees of Rousseau (1762) La Chalotais (1762), and also for the indegrees of all texts who make references

--- analysis_scripts/ad_hoc_analyses.R
+++ analysis_scripts/ad_hoc_analyses.R
@@ -48,6 +48,40 @@ if (text_or_pers == "text") {
   percent_refs_to_rousseau <- sum(
     references_of_texts_62_63["Rousseau (1762)",]/length(unique(texts_62_63$`ACTOR-TEXT`))
     )
-  
+rm(texts_62_63)
 }
 
+
+# display the indegrees and outdegrees of texts referring to Rousseau (1762) and La Chalotais (1763)
+
+ids_referring_to_rousseau <- unique(
+  QDC_es$Actor_code[QDC_es$`TIE-TEXT`=="Rousseau (1762)"])
+names_referring_to_rousseau <- unique(
+  QDC_es$`ACTOR-TEXT`[QDC_es$`TIE-TEXT`=="Rousseau (1762)"])
+
+indegree_texts_referring_to_rousseau <- degree(QDC_text_dyn, nodes = ids_referring_to_rousseau, cmode = "indegree")
+names(indegree_texts_referring_to_rousseau) <- names_referring_to_rousseau
+outdegree_texts_referring_to_rousseau <- degree(QDC_text_dyn, nodes = ids_referring_to_rousseau, cmode = "outdegree")
+names(outdegree_texts_referring_to_rousseau) <- names_referring_to_rousseau
+
+rm(ids_referring_to_rousseau, names_referring_to_rousseau)
+
+
+ids_referring_to_la_chalotais <- unique(
+  QDC_es$Actor_code[QDC_es$`TIE-TEXT`=="La Chalotais (1763)"])
+names_referring_to_la_chalotais <- unique(
+  QDC_es$`ACTOR-TEXT`[QDC_es$`TIE-TEXT`=="La Chalotais (1763)"])
+
+indegree_texts_referring_to_la_chalotais <- degree(QDC_text_dyn, nodes = ids_referring_to_la_chalotais, cmode = "indegree")
+names(indegree_texts_referring_to_la_chalotais) <- names_referring_to_la_chalotais
+outdegree_texts_referring_to_la_chalotais <- degree(QDC_text_dyn, nodes = ids_referring_to_la_chalotais, cmode = "outdegree")
+names(outdegree_texts_referring_to_la_chalotais) <- names_referring_to_la_chalotais
+
+rm(ids_referring_to_la_chalotais, names_referring_to_la_chalotais)
+
+ids_all_texts_making_references <- unique(QDC_es$Actor_code)
+
+indegree_all_texts_making_references <- degree(QDC_text_dyn, nodes = ids_all_texts_making_references, cmode = "indegree")
+indegree_all_texts_making_references <- table(indegree_all_texts_making_references)
+
+rm(ids_all_texts_making_references)
